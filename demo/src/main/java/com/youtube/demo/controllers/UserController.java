69fc93c32989f191e66b9b20b42e8c0e4a7397ca
Grisel30 <60045317+Grisel30@users.noreply.github.com>
2020-03-02 12:48:08 -0600

    Backend
    Backend, create, read, update, and delete.

---
+++ demo/src/main/java/com/youtube/demo/controllers/UserController.java
@@ -0,0 +1,97 @@
+package com.youtube.demo.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.youtube.demo.model.User;
+import com.youtube.demo.service.UserService;
+import com.youtube.demo.util.RestResponse;
+import com.youtube.demo.model.User;
+
+//Interface para el Frontend
+
+//Crear metodo RESt
+@CrossOrigin
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	protected UserService userService;
+	
+	protected ObjectMapper mapper;
+	
+	//Mensaje de respuesta
+	@RequestMapping(value="/saveOrUpdate", method = RequestMethod.POST)
+	public RestResponse saveOrUpdate(@RequestBody String userJson) 
+			throws JsonParseException, JsonMappingException, IOException {
+		this.mapper = new ObjectMapper();
+		
+		User user= this.mapper.readValue(userJson, User.class);
+		
+		if(!this.validate(user)) {
+			return new RestResponse(HttpStatus.NOT_ACCEPTABLE.value(),
+					"Los campos obligatorios no están diligenciados");
+		}
+		this.userService.save(user);
+		return new RestResponse(HttpStatus.OK.value(), "Operacion exitosa");
+	}
+	
+	@GetMapping("/sayHello")
+	public String sayHello(){
+		return "Hello World";
+	}
+	
+	@GetMapping
+	public List<User> getUserList(){
+		return userService.getAll();
+	}
+	
+	@RequestMapping(value="/getUsers", method = RequestMethod.GET)
+	public List<User> getUsers() {
+		return this.userService.findAll();	
+	}
+	
+	//<!--Crear el metodo eliminar
+	@RequestMapping(value="/deleteUsers", method = RequestMethod.POST)
+	public void deleteUsers(@RequestBody String userJson) throws Exception {
+		this.mapper = new ObjectMapper();
+		
+		User user= this.mapper.readValue(userJson, User.class);
+		
+		if (user.getId() == null) {
+			throw new Exception("El id está nulo");
+		}
+		this.userService.deleteUser(user.getId());
+	}
+	
+	private boolean validate(User user) {
+		boolean isValid = true;
+		
+		if(StringUtils.trimToNull(user.getFirstName()) == null) {
+			isValid = false;
+		}
+		
+		if(StringUtils.trimToNull(user.getFirstSurname()) == null) {
+			isValid = false;	
+	}
+		if(StringUtils.trimToNull(user.getAddress()) == null) {
+			isValid = false;
+		}
+		return isValid;
+	}
+}
+
